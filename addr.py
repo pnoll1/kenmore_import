306d7fd6e48fc1c43f348d47d456d45e866823cd
pat <pat@desktop.lan>
2019-11-07 23:47:37 -0800

    added to translation, trimmed addresses

--- addr.py
+++ addr.py
@@ -10,21 +10,35 @@ def translateName(rawname):
     '''
     suffixlookup = {
     'AVE':'Avenue',
+    'CIR':'Circle',
     'RD':'Road',
+    'EXT':'Extension',
     'ST':'Street',
     'PL':'Place',
     'CRES':'Crescent',
     'BLVD':'Boulevard',
     'DR':'Drive',
+    'LN':'Lane',
+    'LN RD':'Lane Road',
     'LANE':'Lane',
+    'LP':'Loop',
+    'CT':'Court',
     'CRT':'Court',
     'GR':'Grove',
     'CL':'Close',
-    'Rwy':'Railway',
-    'Div':'Diversion',
-    'Hwy':'Highway',
-    'Hwy':'Highway',
-    'Conn': 'Connector',
+    'TER': 'Terrace',
+    'TRL':'Trail',
+    'AVE CT': 'Avenue Court',
+    'AVE PL': 'Avenue Place',
+    'ST CT': 'Street Court',
+    'ST PL': 'Street Place',
+    'HL': 'Hill',
+    'VW' : 'View',
+    'PKWY':'Parkway',
+    'RWY':'Railway',
+    'DIV':'Diversion',
+    'HWY':'Highway',
+    'CONN': 'Connector',
     'E':'East',
     'S':'South',
     'N':'North',
@@ -52,8 +66,8 @@ def filterTags(attrs):
     if 'FULLNAME' in attrs and attrs['FULLNAME'] != '':
         tags['addr:street'] = translateName(attrs['FULLNAME'])
     if 'CTYNAME' in attrs and attrs['CTYNAME'] != '':
-        tags['addr:city'] = str.capitalize(attrs['CTYNAME'])
+        tags['addr:city'] = attrs['CTYNAME'].title()
     if 'ZIP5' in attrs and attrs['ZIP5'] != '':
         tags['addr:postcode'] = attrs['ZIP5']
-
+    
     return tags
